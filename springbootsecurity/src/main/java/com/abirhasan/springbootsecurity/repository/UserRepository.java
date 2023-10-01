5250f22db1e496423a40b2225f84bc4018b7f234
Md. Abir Hasan <55059279+Abir835@users.noreply.github.com>
2023-10-01 17:30:40 +0600

    Add files via upload
    authentication with jwt

---
+++ springbootsecurity/src/main/java/com/abirhasan/springbootsecurity/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.abirhasan.springbootsecurity.repository;
+
+import com.abirhasan.springbootsecurity.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
---
+++ springbootsecurity/src/main/java/com/abirhasan/springbootsecurity/user/Role.java
@@ -0,0 +1,6 @@
+package com.abirhasan.springbootsecurity.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
